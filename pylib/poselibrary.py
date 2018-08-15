6ee775a6969708543aea359a8c55764298d4f44b
amanatsu-knit <amanatsu.knit@gmail.com>
2018-08-15 22:43:46 +0900

    modified poselibrary.py

--- pylib/poselibrary.py
+++ pylib/poselibrary.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
-My Pose Library v0.1
+Pose Library v0.1
 Data            : August 11, 2018
 last modified   : August 11, 2018
 Author          : shimono-a
 """
 import sys
+
 sys.dont_write_bytecode = True
 
 from maya import OpenMayaUI as omui
@@ -23,18 +24,42 @@ try:
 except ImportError:
     from pylib.ui.pyside import poselibrary_window
 
-class MainWindow(QtWidgets.QMainWindow, poselibrary_window.Ui_MainWindow):
+
+class PoseLibrary(QtWidgets.QMainWindow, poselibrary_window.Ui_MainWindow):
     def __init__(self, parent=None, *args, **kwargs):
-        super(MainWindow, self).__init__(parent)
+        super(PoseLibrary, self).__init__(parent)
         self.setupUi(self)
+
+        """Global variables"""
+        self.libraryDirectiry = 'D:/work/Maya/PoseLibrary'
+
+        """Call functions"""
+        self.uiConfigure()
+
+    def uiConfigure(self):
+        self.setWindowTitle('Pose Library v0.1')
+
+        """Folder menu"""
+        self.folderMenu = QtGui.QMenu(self)
+        self.folderMenu.addAction(self.action_createFolder)
+
+        """Custom context Menu"""
+        self.treeWidget_folderList.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.treeWidget_folderList.customContextMenuRequested.connect(self.onFolderContextMenu)
+
+    def onFolderContexMenu(self, point):
+        self.folderMenu.exec_(self.treeWidget_folderList.mapToGlobal(point))
+
 
 def maya_main_window():
     maya_mainwindow_ptr = omui.MQtUtil.mainWindow()
 
     return shiboken.wrapInstance(long(maya_mainwindow_ptr), QtWidgets.QMainWindow)
 
+
 def main(*args):
-    dialog = MainWindow(maya_main_window())
+    dialog = PoseLibrary(maya_main_window())
     dialog.show()
+
 
 main()
